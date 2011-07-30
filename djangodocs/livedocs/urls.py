aa8b5f026e0391079f54cdabf0d3f4f23bac8968
piumosso <golikoffartem@gmail.com>
2011-07-30 10:54:51 +0400

    Some css and search form markup

--- djangodocs/livedocs/urls.py
+++ djangodocs/livedocs/urls.py
@@ -3,5 +3,8 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('',
-    url(r'', TemplateView.as_view(template_name='livedocs/index.html')),
+    url(r'^$', TemplateView.as_view(template_name='livedocs/index.html')),
+
+    # Markup
+    url(r'^markup/index/', TemplateView.as_view(template_name='livedocs/markup/index.html')),
 )
