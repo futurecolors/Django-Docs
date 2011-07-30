947f0cb2c360ee5376f203741bf975e58d3ae0ed
piumosso <golikoffartem@gmail.com>
2011-07-30 10:37:57 +0400

    Layout, reset.css

--- djangodocs/livedocs/urls.py
+++ djangodocs/livedocs/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = patterns('',
+    url(r'', TemplateView.as_view(template_name='livedocs/index.html')),
 )
