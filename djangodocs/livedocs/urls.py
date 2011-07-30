7fcc9d3f60a7fdc7ddbae5fae124c864c68cb5b0
Ilya Baryshev <baryshev@gmail.com>
2011-07-30 17:49:41 +0400

    Views converted to template views

--- djangodocs/livedocs/urls.py
+++ djangodocs/livedocs/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.base import TemplateView
+from views import SearchView, ItemView
 
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='livedocs/index.html')),
-    url(r'^search/$', 'livedocs.views.search', name='search'),
+    #url(r'^search/$', 'livedocs.views.search', name='search'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
+    url(r'^(?P<item_path>.*)/$', ItemView.as_view(), name='item'),
 )
--- djangodocs/livedocs/views.py
+++ djangodocs/livedocs/views.py
@@ -1,29 +1,54 @@
 from annoying.decorators import render_to
 from django.core.urlresolvers import resolve, reverse
 from django.db.models.query_utils import Q
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
 from forms import SearchForm
 from models import Item, Version
 
-@render_to('livedocs/search.html')
-def search(request, current_version):
-    """ Backend url for searching items"""
-    search_form = SearchForm(request.GET)
-    if search_form.is_valid():
-        query = search_form.cleaned_data['query']
 
-    items = Item.objects.filter(Q(version__name=current_version), Q(title__icontains=query) | Q(content__icontains=query))
+class BaseLiveView(View):
+    def get_context(self, request, *args, **kwargs):
+        search_form = SearchForm(request.GET)
+        if search_form.is_valid():
+            query = search_form.cleaned_data['query']
+        else:
+            query = None
 
-    match = resolve(request.path)
+        match = resolve(request.path)
+        avaliable_versions = Version.objects.all().order_by('name')
+        for version in avaliable_versions:
+            version.url = reverse(match.url_name, kwargs=kwargs)
+            if version.name == kwargs['current_version']:
+                version.selected = True
 
-    avaliable_versions = Version.objects.all().order_by('name')
-    for version in avaliable_versions:
-        version.url = reverse(match.url_name, kwargs={'current_version': version.name})
-        if version.name == current_version:
-            version.selected = True
+        return {'search_form': search_form,
+                'query': query,
+                'current_version': kwargs['current_version'],
+                'avaliable_versions': avaliable_versions}
 
-    return {'search_form': search_form,
-            'current': current_version,
-            'avaliable_versions': avaliable_versions,
-            'query': query,
-            'items': items,
-            'found_count': len(items)}
+    
+class SearchView(BaseLiveView):
+    @method_decorator(render_to('livedocs/search.html'))
+    def get(self, request, *args, **kwargs):
+        """ Backend url for searching items"""
+
+        context = self.get_context(request, *args, **kwargs)
+
+        context['items'] = Item.objects.filter(Q(version__name=context['current_version']),
+                                    Q(title__icontains=context['query']) | Q(content__icontains=context['query']))
+        context['found_count'] = context['items'].count()
+
+        return context
+
+
+class ItemView(BaseLiveView):
+    @method_decorator(render_to('livedocs/item.html'))
+    def get(self, request, *args, **kwargs):
+        """ Documentation page"""
+
+        context = self.get_context(request, *args, **kwargs)
+        context['item'] = get_object_or_404(Item, path=kwargs['item_path'])
+
+        return context
